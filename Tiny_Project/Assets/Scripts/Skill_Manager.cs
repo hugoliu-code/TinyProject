bfc5c48f575e6d05a256ef2483fcc856da3d45cc
issuehugo <98724522+issuehugo@users.noreply.github.com>
2023-03-05 22:17:30 -0500

    Minor Commenting for clarity

--- Tiny_Project/Assets/Scripts/Skill_Manager.cs
+++ Tiny_Project/Assets/Scripts/Skill_Manager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Skill_Manager : MonoBehaviour
 {
+    /* This script will take care of
+     * Picking up EXP
+     * Leveling up the player
+     * Prompting the Skill select screen
+     * Store Skill trees and all things related (still not properly planned yet)
+     * Give values for game scripts to interpret (like if the "speed upgrade" is chosen, this script should have a indicator that the Player_Movement Script can interpret)
+     */
     //Serialized for now, for Debugging and Testing reasons
     [SerializeField] private int playerLevel = 0;
     [SerializeField] private int neededExp = 10;
