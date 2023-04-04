21883efa8f6e281c53a70d73642b19c183e4c852
Jakkalantern <jakkalantern@gmail.com>
2023-04-03 20:08:51 -0700

    added simple level manager

---
+++ Tiny_Project/Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Manager : MonoBehaviour
+{
+    public static Level_Manager Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public async void LoadScene(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        scene.allowSceneActivation = true;
+    }
+}
